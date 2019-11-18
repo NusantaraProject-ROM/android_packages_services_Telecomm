0638ef29c4c54221b8b0fb12f32f6c6711df6cdc
Zhao Fan <zhaof@codeaurora.org>
2019-11-18 17:54:49 +0800

    Fix audio mode issue after conference call failed
    Device is always in earpiece mode after conference call failed
    if SIM card not support conference calL. Fix the issue by Adding
    handling for CallState.NEW to onCallLeavingState() in CallAudioManager.
    
    Change-Id: Id155c7d70652eec66babbfa3ebe0575535a88f87
    CRs-Fixed: 2555788

--- src/com/android/server/telecom/CallAudioManager.java
+++ src/com/android/server/telecom/CallAudioManager.java
@@ -561,6 +561,7 @@ public class CallAudioManager extends CallsManagerListenerBase {
                 onCallLeavingActiveDialingOrConnecting();
                 break;
             case CallState.DIALING:
+            case CallState.NEW:
                 stopRingbackForCall(call);
                 onCallLeavingActiveDialingOrConnecting();
                 break;
