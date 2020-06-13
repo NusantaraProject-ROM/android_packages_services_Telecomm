8f2734a39188a830fcd39125a1e575d7a24d9a85
Ido Ben-Hur <idoybh2@gmail.com>
2020-06-13 03:29:55 +0300

    Telecomm: Add DND & Rate settings to flash on call [2/3]

--- src/com/android/server/telecom/Ringer.java
+++ src/com/android/server/telecom/Ringer.java
@@ -394,6 +394,15 @@ public class Ringer {
             }
         }
 
+        boolean ignoreDND = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.FLASHLIGHT_ON_CALL_IGNORE_DND, 0,
+                UserHandle.USER_CURRENT) == 1;
+        if (!ignoreDND && shouldFlash) { // respect DND
+            int zenMode = Settings.Global.getInt(mContext.getContentResolver(),
+                    Settings.Global.ZEN_MODE, Settings.Global.ZEN_MODE_OFF);
+            shouldFlash = zenMode == Settings.Global.ZEN_MODE_OFF;
+        }
+
         if (shouldFlash) {
             blinkFlashlight();
         }
@@ -756,7 +765,7 @@ public class Ringer {
 
         private boolean shouldStop = false;
         private CameraManager cameraManager;
-        private int duration = 500;
+        private int duration;
         private boolean hasFlash = true;
         private Context context;
 
@@ -768,6 +777,8 @@ public class Ringer {
         private void init() {
             cameraManager = (CameraManager) context.getSystemService(Context.CAMERA_SERVICE);
             hasFlash = context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+            duration = 500 / Settings.System.getIntForUser(context.getContentResolver(),
+                    Settings.System.FLASHLIGHT_ON_CALL_RATE, 1, UserHandle.USER_CURRENT);
         }
 
         void stop() {
