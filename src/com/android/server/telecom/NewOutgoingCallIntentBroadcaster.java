77cc1497b1712e23032d6796fcce5fabe3e57c48
Chinmay Dhodapkar <chinmayd@google.com>
2022-12-04 09:46:34 -0800

    DO NOT MERGE do not process content uri in call Intents
    Add checks in Telecom so content uris are not processed
    
    Bug: 257030107
    Test: atest NewOutgoingCallIntentBroadcasterTest
    Test: adb shell am start -a android.intent.action.CALL -d tel:xxx
    Change-Id: Ic2c3014cecfd5db84dc2023b4c247d96ad1c3414
    Merged-In: Ic2c3014cecfd5db84dc2023b4c247d96ad1c3414
    (cherry picked from commit 9636518478fb887dd1834c0433eb3a71eb72faaf)
    Merged-In: Ic2c3014cecfd5db84dc2023b4c247d96ad1c3414

--- src/com/android/server/telecom/NewOutgoingCallIntentBroadcaster.java
+++ src/com/android/server/telecom/NewOutgoingCallIntentBroadcaster.java
@@ -322,8 +322,18 @@ public class NewOutgoingCallIntentBroadcaster {
     }
 
     private String getNumberFromCallIntent(Intent intent) {
-        String number;
-        number = mPhoneNumberUtilsAdapter.getNumberFromIntent(intent, mContext);
+        String number = null;
+
+        Uri uri = intent.getData();
+        if (uri != null) {
+            String scheme = uri.getScheme();
+            if (scheme != null) {
+                if (scheme.equals("tel") || scheme.equals("sip")) {
+                    number = uri.getSchemeSpecificPart();
+                }
+            }
+        }
+
         boolean isConferenceUri = intent.getBooleanExtra(
                 TelephonyProperties.EXTRA_DIAL_CONFERENCE_URI, false);
         if (!isConferenceUri && TextUtils.isEmpty(number)) {
